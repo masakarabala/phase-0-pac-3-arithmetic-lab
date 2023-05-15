a6df19601f43c6e902431dda2820a24e4a7ccbe3
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-15 19:25:05 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(num1, num2){
+    return num1+num2;
+    }
+    console.log(add(1,2));
+
+function subtract(num1, num2){
+        return num1-num2;
+        }
+        console.log(subtract(2,2));
+
+function multiply(num1, num2){
+         return num1*num2;
+        }
+        console.log(multiply(2,2)); 
+
+function divide(num1, num2){
+        return num1/num2;
+        } 
+        console.log(divide(4,2)); 
+
+function increment(n){
+         n++
+         return n;
+          } 
+          console.log(increment(2)); 
+
+function decrement(n){
+    n--
+    return n;
+     } 
+    console.log(decrement(3))
+    
+function makeInt(n){
+     return parseInt(n, 10);
+     }
+    console.log(makeInt("5.9", 10));
+
+    function preserveDecimal(n){
+        return parseFloat(n);
+        }
+       console.log(preserveDecimal("5.9777"));
+   
